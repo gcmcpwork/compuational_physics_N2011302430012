d52ca207031c4b41c55ccefefd65e27c35e776d9
gcmcpwork <285302826@qq.com>
2016-09-26 08:00:31 +0800

    Add files via upload

---
+++ Exercise_03/hahaha.py
@@ -0,0 +1,37 @@
+import time
+import os
+i=0
+for i in range(100):
+    if i%2==0:
+        os.system('cls')
+        print' 	                                 '
+        print' 	                                 '
+        print' 	                                      '
+        print' 	                                      '
+        print' 	                                      '
+        print' 	                                      '
+        print' 	                AAAAAAAAAA      '
+        print' 	              A        A A      '
+        print' 	            AAAAAAAAAA   A      '
+        print' 	            A        A   A      '
+        print' 	            A        A   A      '
+        print' 	            A        A A        '
+        print' 	            AAAAAAAAAA          '
+        time.sleep(0.3)
+    else:
+        os.system('cls')
+        print' 	                                 '
+        print' 	                                 '
+        print' 	                                      '
+        print' 	                                      '
+        print' 	                                 '
+        print'	                      A       '
+        print'	                   A    A     '
+        print'	                A         A   '
+        print'	              A   A         A '
+        print'	            A       A    AA   '
+        print'	          A           A A     '
+        print'	            A       A A       '
+        print'	              A   AA          '
+        print'	                A             '
+        time.sleep(0.3)
